6b02179e68012a772ebf55e8636b73a9ae74c614
duvan <duvanmmontoya@gmail.com>
2024-03-22 16:32:04 -0500

    star proyect

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProyectBiblio.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
